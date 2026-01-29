1d9cf401b29be248ba45c207e5758acefc66c76a
Ahmil Jilani <hypehuman@users.noreply.github.com>
2026-01-29 13:52:45 +0100

    new .NET 10 console app

---
+++ VsSolutionDir/ChaseZelleReader/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ChaseZelleReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
